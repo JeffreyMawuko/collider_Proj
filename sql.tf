de33bc06db902f7402621723056689f5dcfe14c0
MawukoJeffrey-JrAinMath <jeffmawuko@gmail.com>
2025-08-16 00:39:52 +0100

    Update sql.tf

--- sql.tf
+++ sql.tf
@@ -43,3 +43,70 @@ resource "azurerm_key_vault_secret" "db_connstr" {
   value        = "Server=tcp:${azurerm_mssql_server.sql.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${random_password.sql_admin.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
   key_vault_id = azurerm_key_vault.kv.id
 }
+
+# Private DNS + Private Endpoints for SQL & KV
+resource "azurerm_private_dns_zone" "sql" {
+  name                = "privatelink.database.windows.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "sql_vnet" {
+  name                  = "${var.prefix}-sqlzone-link-${random_string.sfx.result}"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.sql.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_private_endpoint" "sql" {
+  name                = "${var.prefix}-pe-sql-${random_string.sfx.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.backend.id
+  tags                = local.tags
+
+  private_service_connection {
+    name                           = "${var.prefix}-sql-privlink"
+    private_connection_resource_id = azurerm_mssql_server.sql.id
+    subresource_names              = ["sqlServer"]
+    is_manual_connection           = false
+  }
+}
+
+resource "azurerm_private_dns_zone_group" "sql" {
+  name                 = "${var.prefix}-sql-dnsgrp"
+  private_endpoint_id  = azurerm_private_endpoint.sql.id
+  private_dns_zone_ids = [azurerm_private_dns_zone.sql.id]
+}
+
+resource "azurerm_private_dns_zone" "kv" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "kv_vnet" {
+  name                  = "${var.prefix}-kvzone-link-${random_string.sfx.result}"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.kv.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_private_endpoint" "kv" {
+  name                = "${var.prefix}-pe-kv-${random_string.sfx.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.backend.id
+  tags                = local.tags
+
+  private_service_connection {
+    name                           = "${var.prefix}-kv-privlink"
+    private_connection_resource_id = azurerm_key_vault.kv.id
+    subresource_names              = ["vault"]
+    is_manual_connection           = false
+  }
+}
+
+resource "azurerm_private_dns_zone_group" "kv" {
+  name                 = "${var.prefix}-kv-dnsgrp"
+  private_endpoint_id  = azurerm_private_endpoint.kv.id
+  private_dns_zone_ids = [azurerm_private_dns_zone.kv.id]
+}
