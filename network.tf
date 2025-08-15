861391359e8694989b69c1f211c6f5132462f557
MawukoJeffrey-JrAinMath <jeffmawuko@gmail.com>
2025-08-16 00:36:07 +0100

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,42 @@
+hcl
+resource "azurerm_virtual_network" "vnet" {
+  name                = "ProdvNet" # per request (note case)
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = var.address_space
+  tags                = local.tags
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = "backend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.backend_subnet_cidr]
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "frontend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.frontend_subnet_cidr]
+}
+
+# Subnet dedicated for App Service VNet Integration
+resource "azurerm_subnet" "appsvc" {
+  name                 = "appsvc"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.appsvc_subnet_cidr]
+
+  delegation {
+    name = "webapp_delegation"
+    service_delegation {
+      name = "Microsoft.Web/serverFarms"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
+        "Microsoft.Network/virtualNetworks/subnets/action"
+      ]
+    }
+  }
+}
